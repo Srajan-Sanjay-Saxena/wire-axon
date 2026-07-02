dfe8a7b8ba8d03386d9256ff9cd283cfa2b514ee
Srajan Saxena <srajan@zenith.dev>
2026-07-02 19:10:35 +0530

    auth: add barrel export for all auth strategies and ConsoleLogger

--- auth/index.ts
+++ auth/index.ts
@@ -1,11 +1,4 @@
-export { BaseAuthAxios } from '@base/base.auth..js';
-export { BearerTokenStrategy } from '@auth/strategy/jwt.bearer.token.strategy.js';
-export { CookieStrategy } from '@auth/strategy/jwt.cookie.strategy.js';
-export { RefreshTokenStrategy } from '@auth/strategy/access-refresh.token.strategy.js';
-export { ConsoleLogger } from '@auth/logger/logger.adapter.js';
-export type { LoggerAdapter, LoggerConfig, BaseAuthConfig } from '@config/auth.config.js';
-export type {
-  RefreshTokenConfig,
-  FailedRequestConfig,
-  InterceptedRequestModifiedAxiosError,
-} from '@lib/axios-custom.types.js';
+export { BearerTokenStrategy } from './strategy/jwt.bearer.token.strategy.js';
+export { CookieStrategy } from './strategy/jwt.cookie.strategy.js';
+export { RefreshTokenStrategy } from './strategy/access-refresh.token.strategy.js';
+export { ConsoleLogger } from './logger/logger.adapter.js';
