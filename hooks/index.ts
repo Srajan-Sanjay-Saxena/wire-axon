51c49948168b6cc455153ea434dc85fb769c79f6
Srajan Saxena <srajan@zenith.dev>
2026-07-02 19:10:35 +0530

    hooks: add barrel export for all 4 hooks

---
+++ hooks/index.ts
@@ -0,0 +1,4 @@
+export { useApiQuery } from './useQueryApiService.js';
+export { useApiMutation } from './useMutationApiService.js';
+export { useScratchQuery } from './useScratchServiceQuery.js';
+export { useScratchMutation } from './useScratchServiceMutation.js';
