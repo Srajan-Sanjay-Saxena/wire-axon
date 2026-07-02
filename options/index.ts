705bf545763fec194c6789308a16d152c5905e8e
Srajan Saxena <srajan@zenith.dev>
2026-07-02 19:10:35 +0530

    options: add barrel export for DEFAULT_RETRY_CONFIG and TanStack Query default options

---
+++ options/index.ts
@@ -0,0 +1,2 @@
+export { DEFAULT_RETRY_CONFIG } from './retry.default.options.js';
+export { TANSTACK_QUERY_DEFAULT_OPTIONS, TANSTACK_MUTATION_DEFAULT_OPTIONS } from './tanstack.query.default.options.js';
