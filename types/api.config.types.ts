b1fc6c2fe3b0dc7c58f4c700f30a0a73a926f1b8
Srajan Saxena <srajan@zenith.dev>
2026-07-02 19:10:34 +0530

    types: make headers optional in ApiConfig

--- types/api.config.types.ts
+++ types/api.config.types.ts
@@ -4,9 +4,9 @@ type HttpMethodsType = 'get' | 'post' | 'delete' | 'patch';
 type AllowedKeysType = `${HttpMethodsType}:${string}`;
 
 /** Subset of axios request config exposed to consumers */
-type ApiConfig = Pick<
+type ApiConfig = Omit<Pick<
   InternalAxiosRequestConfig,
   'headers' | 'params' | 'timeout' | 'signal'
->;
+>, 'headers'> & { headers?: InternalAxiosRequestConfig['headers'] };
 
 export type { HttpMethodsType, AllowedKeysType, ApiConfig };
